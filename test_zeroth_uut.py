11769911e9663f118f628ec2a3261f8b3f54ebe4
Jeffrey Silverman <jsilverman@juniper.net>
2020-04-03 02:02:20 -0700

    Added some tests (which all fail)

--- test_zeroth_uut.py
+++ test_zeroth_uut.py
@@ -2,18 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-import first_uut
+from zeroth_uut import *
 
 def test_add_2_always_right():
+    assert add_2_always_right(2, 2) == 4
     pass
 
 def test_add_2_always_wrong ():
+    assert add_2_always_wrong(2, 2) == 4
     pass
 
 def test_add_2_sometimes_wrong():
+    assert add_2_sometimes_wrong(2, 2) == 4
     pass
 
-def add_2_always_right_strongly_typed():
+def test_add_2_always_right_strongly_typed():
+    assert add_2_always_right_strongly_typed(2, 2) == 4
     pass
 
 
