1e4881cad5cac57afae55b8cbaf8f9fef21f4c26
Jeffrey Silverman <jsilverman@juniper.net>
2020-04-04 03:35:32 -0700

    Added more test cases

--- test_first_uut.py
+++ test_first_uut.py
@@ -1,23 +1,29 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pytest
 from first_uut import *
+
 
 def test_add_2_always_right():
     assert add_2_always_right(2, 2) == 4
-    pass
+    assert add_2_always_right(2, 3) == 5
+    assert add_2_always_right(3, 3) == 6
 
-def test_add_2_always_wrong ():
+
+def test_add_2_always_wrong():
     assert add_2_always_wrong(2, 2) == 4
-    pass
+    assert add_2_always_wrong(2, 3) == 5
+    assert add_2_always_wrong(3, 3) == 6
+
 
 def test_add_2_sometimes_wrong():
     assert add_2_sometimes_wrong(2, 2) == 4
-    pass
+    assert add_2_sometimes_wrong(2, 3) == 5
+    assert add_2_sometimes_wrong(3, 3) == 6
+
 
 def test_add_2_always_right_strongly_typed():
     assert add_2_always_right_strongly_typed(2, 2) == 4
-    pass
-
+    assert add_2_always_right_strongly_typed(2, 3) == 5
+    assert add_2_always_right_strongly_typed(3, 3) == 6
 
